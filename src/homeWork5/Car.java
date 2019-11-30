83fa62a9e23db7c7e4a0abea1ff8eb18aa8127af
KonstantinGlady <gladyi@hotmail.com>
2019-11-30 19:18:45 +0300

    homeWork

--- src/homeWork5/Car.java
+++ src/homeWork5/Car.java
@@ -1,7 +1,12 @@
 package homeWork5;
 
-public class Car implements Runnable {
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Lock;
+
+public class Car implements Callable<String> {
     private static int CARS_COUNT;
+
 
     static {
         CARS_COUNT = 0;
@@ -10,6 +15,9 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private CountDownLatch latch;
+    private CountDownLatch latch2;
+    private Lock lock;
 
     public String getName() {
         return name;
@@ -19,24 +27,35 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CountDownLatch latch) {
         this.race = race;
         this.speed = speed;
+        this.latch = latch;
+        this.latch2 = latch;
+
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
 
     @Override
-    public void run() {
+    public String call() throws Exception {
         try {
+
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
+            latch.countDown();
+            latch.await();
+
             System.out.println(this.name + " готов");
+            latch2.countDown();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        latch2.await();
+        
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        return this.name + " DONE";
     }
 }
